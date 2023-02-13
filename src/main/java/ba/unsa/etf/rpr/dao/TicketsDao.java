ff10fb70ff9f9898f2bd7a6661fd85eb3f215907
VedadHajric <vhajric1@etf.unsa.ba>
2023-02-13 18:25:41 +0100

    TicketsDao interface

---
+++ src/main/java/ba/unsa/etf/rpr/dao/TicketsDao.java
@@ -0,0 +1,10 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Tickets;
+
+/**
+ * Dao interface for Reservations domain bean
+ *
+ */
+public interface TicketsDao extends Dao<Tickets>{
+}
